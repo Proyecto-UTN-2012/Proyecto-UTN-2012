e3f63bb72045245a05ed8797e5d171c2f5ed9584
Marcelo Pasut <pasutmarcelo@gmail.com>
2012-05-31 17:26:43 -0300

    Armado de proyecto android

---
+++ integrar-t-android/gen/org/utn/proyecto/helpful/integrart/integrar_t_android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.utn.proyecto.helpful.integrart.integrar_t_android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
