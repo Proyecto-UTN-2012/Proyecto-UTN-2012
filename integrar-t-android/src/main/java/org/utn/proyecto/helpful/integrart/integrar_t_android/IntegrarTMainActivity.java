b042cb896cea34f80a9cfae031db4be20a878895
Ariel Salazar <Will@Ariels-MacBook-Pro.local>
2012-07-09 20:38:09 -0300

    Delete

--- integrar-t-android/src/main/java/org/utn/proyecto/helpful/integrart/integrar_t_android/IntegrarTMainActivity.java
+++ integrar-t-android/src/main/java/org/utn/proyecto/helpful/integrart/integrar_t_android/IntegrarTMainActivity.java
@@ -14,7 +14,7 @@ public class IntegrarTMainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.mainmenu);
     }
 
 }
