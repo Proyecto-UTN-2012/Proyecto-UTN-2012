4c3b2e23048beb7d4472fdbe8ce9d2178b2a8ed0
Ariel Salazar <Will@Ariels-MacBook-Pro.local>
2012-10-21 11:13:01 -0300

    conociendo a cali

---
+++ integrar-t-android/src/main/java/org/utn/proyecto/helpful/integrart/integrar_t_android/activities/conociendoacali/ConociendoACaliActivity.java
@@ -0,0 +1,18 @@
+package org.utn.proyecto.helpful.integrart.integrar_t_android.activities.conociendoacali;
+
+import org.utn.proyecto.helpful.integrart.integrar_t_android.R;
+
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
+import android.os.Bundle;
+
+@ContentView(R.layout.cac_seleccion_personaje)
+public class ConociendoACaliActivity extends RoboActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+    }
+
+}
